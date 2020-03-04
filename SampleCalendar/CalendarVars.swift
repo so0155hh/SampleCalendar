16ab9533d04b4950e23137af31fb838de9a778be
Nozomi <nozomi.kuwahara@gmail.com>
2020-03-04 09:04:55 +0900

    Calendar

---
+++ SampleCalendar/CalendarVars.swift
@@ -0,0 +1,9 @@
+//
+//  CalendarVars.swift
+//  SampleCalendar
+//
+//  Created by 桑原望 on 2020/02/18.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import Foundation
---
+++ SampleCalendar/DateCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  DateCollectionViewCell.swift
+//  SampleCalendar
+//
+//  Created by 桑原望 on 2020/02/18.
+//  Copyright © 2020 MySwift. All rights reserved.
+//
+
+import UIKit
+
+class DateCollectionViewCell: UICollectionViewCell {
+    
+}
